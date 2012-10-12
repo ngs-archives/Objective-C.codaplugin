b212cad8ace92c0e0c85af0d67c819b86560db02
Atsushi NAGASE <atsn.ngs@gmail.com>
2012-10-13 08:45:01 +0900

    compat with new compiler

--- Contents/Resources/7820D179-D916-4B5A-B3B3-2C873C4995E5/create_snippets.pl
+++ Contents/Resources/7820D179-D916-4B5A-B3B3-2C873C4995E5/create_snippets.pl
@@ -5,7 +5,6 @@ use warnings;
 use Data::Dumper;
 
 my $code = $ENV{CODA_SELECTED_TEXT} || join '', <STDIN>;
-my @synthesize;
 my @encode_with_coder;
 my @init_with_coder;
 my @copy_with_zone;
@@ -13,6 +12,7 @@ my @init_with_dictionary;
 my @includes;
 my @header;
 my @dictionary;
+my $copying = 1;
 
 sub decamelize {
 	my $s = shift;
@@ -36,16 +36,15 @@ while($code =~ /([^\n]+)\n?/g) {
   if(!$class_name && $line =~ /^\/\//) {
     push @header, $line;
   }
-  if(!$class_name && $line =~ /\@interface\s+([^:\s]+).+/) {
+  if(!$class_name && $line =~ /\@interface\s+([^:\s]+)(.+)/) {
     $class_name = $1;
-    push @includes, "#include \"${class_name}.h\"";
+    $copying = $2 !~ /NSCopying/;
+    push @includes, "#import \"${class_name}.h\"";
     next;
   }
-  my ( $attr, $type, $varname ) = $line =~ m/\@property\s*\(([\w,]+)\)\s*([^\s]+)\s*\*?([^;]+);/;
+  my ( $attr, $type, $varname ) = $line =~ m/\@property\s*\(([\w,\s]+)\)\s*([^\s]+)\s*\*?([^;]+);/;
   next if !$varname || $attr =~ /readonly/;
   my $encode_method = undef;
-  
-  push @init_with_dictionary, "    if([dictionary objectForKey:@\"${varname}\"])";
   
   if($attr =~ /copy/) {
     $encode_method = 'Object';
@@ -73,41 +72,53 @@ while($code =~ /([^\n]+)\n?/g) {
   }
   my $is_object = $encode_method eq 'Object';
   my $is_foundation = $is_object && $type =~ /^NS/;
+  my $is_nsurl = $type eq 'NSURL';
   my $key = decamelize($varname);
+  
+  my $if = "[dictionary[@\"${key}\"] isKindOfClass:[NSString class]]";
+  $if = "($if ||\n[dictionary[@\"${key}\"] isKindOfClass:[NSNumber class]])" unless $is_object;
+  push @init_with_dictionary, "    if(dictionary[@\"${key}\"]&&\n${if})";
   
   ( my $valmethod = lc $encode_method ) =~ s/\d+//g;
   
   my ($in_var, $out_var);
-  if($is_foundation) {
-    $in_var = "[dictionary valueForKey:@\"${key}\"]";
+  if($is_nsurl) {
+    $in_var = "[NSURL URLWithString:dictionary[@\"${key}\"]]";
+    $out_var = "self.${varname}.absoluteString";
+  } elsif($is_foundation) {
+    $in_var = "dictionary[@\"${key}\"]";
     $out_var = "self.${varname}";
   } elsif($is_object) {
-    $in_var = "[[${type} alloc] initWithDictionary:[dictionary objectForKey:@\"${key}\"]]";
+    $in_var = "[[${type} alloc] initWithDictionary:dictionary[\@\"${key}\"]]";
     $out_var = "[self.${varname} dictionary]";
   } else {
-    $in_var = "[[dictionary valueForKey:@\"${key}\"] ${valmethod}Value];";
-    $out_var = "[NSNumber numberWith${encode_method}:self.${varname}]";
+    $in_var = "[dictionary[\@\"${key}\"] ${valmethod}Value]";
+    $out_var = "\@\(self.${varname}\)";
   }
   push @init_with_dictionary, "      self.${varname} = $in_var;";
-  push @dictionary, "          ${out_var}, @\"${key}\",";
-  push @synthesize, "\@synthesize $varname = _$varname;";
-  push @includes, "#include \"${type}.h\""  if(!$is_foundation && $is_object );
+  push @dictionary, "          \@\"${key}\" : ${out_var}";
+  push @includes, "#import \"${type}.h\""  if(!$is_foundation && $is_object );
   if($encode_method) {
-    push @encode_with_coder, "  [aCoder encode${encode_method}:self.${varname} forKey:@\"${key}\"];";
-    push @init_with_coder, "    self.${varname} = [aDecoder decode${encode_method}ForKey:@\"${key}\"];";
+    push @encode_with_coder, "  [aCoder encode${encode_method}:self.${varname} forKey:\@\"${key}\"];";
+    push @init_with_coder, "    self.${varname} = [aDecoder decode${encode_method}ForKey:\@\"${key}\"];";
   }
 }
 
-my $synthesize           = join("\n", @synthesize);
 my $encode_with_coder    = join("\n", @encode_with_coder);
 my $init_with_coder      = join("\n", @init_with_coder);
 my $copy_with_zone       = join("\n", @copy_with_zone);
 my $init_with_dictionary = join("\n", @init_with_dictionary);
 
 my $includes   = join("\n", @includes);
-my $dictionary = join("\n", @dictionary);
+my $dictionary = join(",\n", @dictionary);
 
 ( my $header = join("\n", @header) ) =~ s/\/\/(\s*)$class_name\.h/\/\/$1$class_name.m/;
+
+my $super_init_with_dictionary = $copying ? '[super initWithDictionary:dictionary]' : '[super init]';
+my $super_init_with_coder = $copying ? '[super initWithCoder:aDecoder]' : '[super init]';
+my $super_encode_with_coder = $copying ? '  [super encodeWithCoder:aCoder];' : '';
+my $super_copy_with_zone = $copying ? '[super copyWithZone:zone]' : '[[[self class] alloc] init]';
+
 
 my $output =<<EOM;
 $header
@@ -116,37 +127,33 @@ $includes
 
 \@implementation $class_name
 
-#pragma mark - Synthesize Accessors
-
-$synthesize
-
 #pragma mark - Dictionary
 
 - (id)initWithDictionary:(NSDictionary *)dictionary {
-  if(self=[super initWithDictionary:dictionary]) {
+  if(self=$super_init_with_dictionary) {
 $init_with_dictionary
   }
   return self;
 }
 
 - (NSDictionary *)dictionary {
-  return [NSDictionary dictionaryWithObjectsAndKeys:
+  return \@{
 $dictionary
-          nil];
+  };
 }
 
 
 #pragma mark - NSCoding
 
 - (id)initWithCoder:(NSCoder *)aDecoder {
-  if(self=[super initWithCoder:aDecoder]) {
+  if(self=$super_init_with_coder) {
 $init_with_coder
   }
   return self;
 }
 
 - (void)encodeWithCoder:(NSCoder *)aCoder {
-  [super encodeWithCoder:aCoder]; 
+$super_encode_with_coder
 $encode_with_coder
 }
 
@@ -154,7 +161,7 @@ $encode_with_coder
 
 
 - (id)copyWithZone:(NSZone *)zone {
-  $class_name* newCopy = [super copyWithZone:zone];
+  $class_name* newCopy = $super_copy_with_zone;
 $copy_with_zone
   return newCopy;
 }
